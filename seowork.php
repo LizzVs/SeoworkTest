0e47955528a386d0904c9e89891a8e62893b39fb
Vasileva <eliz.vasilieva@yandex.ru>
2024-04-23 17:56:18 +0300

    Add Way 1

--- seowork.php
+++ seowork.php
@@ -1,11 +1,21 @@
 <?php 
+// Функция для генерации 50-значных чисел
+function GetBigNumber():float {
+    $str = rand(1,9);
+    for ($i = 1; $i < 3; $i++) {
+        $str .= rand(0,9);
+    }
+    return $str;
+}
 
-$numbers = Array(123, 123, 123, 123, 123);
+// Создание массива с 50 элементами
+$a = Array();
+for ($i = 0; $i < 10; $i++) {
+    array_push($a, GetBigNumber());
+    echo $a[$i] . "\n";
+}
 
-// способ 1
+// Способ 1
+// Сложение чисел через array_sum
+echo sprintf("%.0F", array_sum($a));
 
-echo array_sum($numbers);
-
-// способ 2
-
-// способ 3
