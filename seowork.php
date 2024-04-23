f214b3d06c569b57fa7bedb946892ebf8e2910c4
Vasileva <eliz.vasilieva@yandex.ru>
2024-04-23 18:16:06 +0300

    Add way 2

--- seowork.php
+++ seowork.php
@@ -1,21 +1,31 @@
 <?php 
 // Функция для генерации 50-значных чисел
-function GetBigNumber():float {
+function GetBigNumber() {
     $str = rand(1,9);
-    for ($i = 1; $i < 3; $i++) {
+    for ($i = 1; $i < 50; $i++) {
         $str .= rand(0,9);
     }
     return $str;
 }
 
-// Создание массива с 50 элементами
+// Создание и вывод массива с 50 элементами
 $a = Array();
-for ($i = 0; $i < 10; $i++) {
+echo "Массив: \n";
+for ($i = 0; $i < 50; $i++) {
     array_push($a, GetBigNumber());
     echo $a[$i] . "\n";
 }
 
 // Способ 1
 // Сложение чисел через array_sum
-echo sprintf("%.0F", array_sum($a));
+echo "Способ 1: Сумма - " . sprintf("%.0F", array_sum($a));
+
+// Способ 2
+// Сложение с помощью библиотеки BCMath, позволяющей работать с большими числами
+$sum = 0;
+for ($i = 0; $i < 50; $i++) {
+    $sum = bcadd($sum, $a[$i]);
+}
+echo "\nСпособ 2: Сумма - " . $sum;
+
 
